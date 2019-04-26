1e2b2df2dc9e3a3c0b516d0cc44d97018245e7c6
Eleno <ivan.y.fang@outlook.com>
2019-04-26 21:29:32 +0800

    Shangji-3 ex-1

--- DataStructure/Source/Shangji-3/Forest.h
+++ DataStructure/Source/Shangji-3/Forest.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Stack.h"
+#include "Queue.h"
 using namespace eleno;
 
 template <class T>
@@ -9,12 +10,69 @@ struct Node
 	Node* first_child;
 	Node* next_sibling;
 };
+
 template <class T>
-void InorderTraverse(Node<T>* root)
+void InorderTraverse(Node<T>* node, void* visit(Node<T>*))
 {
-	Stack<Node<T>*> st;
-
-	if (!root)
+	if (!node)
 		return;
+	Stack<Node<T>*> st;
+	while (!st.empty() || node)
+	{
+		if (node)
+		{
+			st.push(node);
+			node = node->first_child;
+		}
+		else
+		{
+			node = st.top();
+			st.pop();
+			visit(node);
+			node = node->next_sibling;
+		}
+	}
+}
 
+template <class T>
+int GetLeafNum(Node<T>* root)
+{
+	if (!root)
+		return 0;
+	int num = 0;
+	auto f = [&num](Node<T>* node)-> void
+	{
+		if (!node->first_child)
+			++num;
+	};
+	InorderTraverse(root, f);
+	return num;
+}
+
+template <class T>
+bool IsComplete(Node<T>* root)
+{
+	// Level traverse the tree,
+	// once encounter a node which is nullptr
+	// any nodes encountered after must be nullptr
+	if (!root)
+		return true;
+	bool done = false;
+	Queue<Node<T>*> q;
+	q.push(root);
+	while (!q.empty())
+	{
+		Node<T>* curr = q.front();
+		q.pop();
+		if (!curr)
+			done = true;
+		else
+		{
+			if (done)
+				return false;
+			q.push(curr->left);
+			q.push(curr->right);
+		}
+	}
+	return true;
 }
---
+++ DataStructure/Source/Shangji-3/Main.cpp
@@ -0,0 +1,10 @@
+#ifdef _FOREST_
+#include <iostream>
+#include "Forest.h"
+int main()
+{
+	Node<char> 
+}
+#endif
+#ifdef _GRAPH_
+#endif
